1da1d1ec4e12a23af6a93c75a9b08b1294765e0e
GigioGigetto <49612477+GigioGigetto@users.noreply.github.com>
2019-07-23 22:54:54 +0200

    Add files via upload
    We added the first MATLAB configuration file and the initial Simulink model, which is able to replay the log data and show the main messages.

---
+++ myLogPlayerProva.m
@@ -0,0 +1,9 @@
+close all
+clear
+clc
+
+canMsgTimetable = canMessageImport('log_0703_radars.asc', 'Vector', 'OutputFormat', 'timetable');
+save log_0703_radars.mat canMsgTimetable;
+canMsgs = canMessageReplayBlockStruct(canMsgTimetable);
+save replay.mat canMsgs;
+whos
