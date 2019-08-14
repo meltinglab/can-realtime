60059607e840024273bc0b91fc23b220e4464427
GigioGigetto <49612477+GigioGigetto@users.noreply.github.com>
2019-08-14 13:16:39 +0200

    We added sensor locations and CRC-8

--- canTrasceiverConfig.m
+++ canTrasceiverConfig.m
@@ -6,4 +6,10 @@ canMsgTimetable = canMessageImport('canalyzerLog0703_radars.asc', 'Vector', 'Out
 %save log_0703_radars.mat canMsgTimetable;
 canMsgs = canMessageReplayBlockStruct(canMsgTimetable);
 save canalyzerLog0703_radars.mat canMsgs;
-%whos
+
+% Sensors location and orientation
+sensorLocationConfig = [hex2dec('00')   hex2dec('00')    hex2dec('00')    hex2dec('00')    hex2dec('00')    hex2dec('00')    hex2dec('00')    hex2dec('00');
+                        hex2dec('00')   hex2dec('00')    hex2dec('00')    hex2dec('00')    hex2dec('00')    hex2dec('00')    hex2dec('00')    hex2dec('00');
+                        hex2dec('FD')   hex2dec('80')    hex2dec('FF')    hex2dec('A5')    hex2dec('FE')    hex2dec('DA')    hex2dec('00')    hex2dec('00');
+                        hex2dec('DD')   hex2dec('40')    hex2dec('FF')    hex2dec('3F')    hex2dec('FC')    hex2dec('F8')    hex2dec('00')    hex2dec('00')];
+sensorLocationConfig = uint8(sensorLocationConfig);
